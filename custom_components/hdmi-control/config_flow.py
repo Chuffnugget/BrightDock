4afc1ee83f54e8c848eefb4e27a2a6b017be98ae
Chuffnugget <88729673+Chuffnugget@users.noreply.github.com>
2025-06-17 20:11:41 +0100

    Update config_flow.py

--- custom_components/hdmi-control/config_flow.py
+++ custom_components/hdmi-control/config_flow.py
@@ -1,8 +1,8 @@
 # File: config_flow.py
-# Description: Python file for managing the HDMI-Control devices and integration setup.
+# Description: Python file for managing the HDMI Control devices and integration setup.
 # Author: Chuffnugget
-from __future__ import annotations
 
+from __future__ import annotations
 import voluptuous as vol
 from typing import Any
 
@@ -14,8 +14,8 @@ from homeassistant.data_entry_flow import FlowResult
 
 from .const import DOMAIN, DEFAULT_PORT
 
-class DDCCIConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
-    """Handle a config flow for HDMI-Control integration."""
+class HDMIControlConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
+    """Handle a config flow for HDMI Control integration."""
 
     VERSION = 1
     CONNECTION_CLASS = config_entries.CONN_CLASS_LOCAL_POLL
@@ -32,7 +32,7 @@ class DDCCIConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
             self._abort_if_unique_id_configured()
 
             return self.async_create_entry(
-                title=f"HDMI-Control @ {user_input[CONF_HOST]}:{user_input[CONF_PORT]}",
+                title=f"HDMI Control @ {user_input[CONF_HOST]}:{user_input[CONF_PORT]}",
                 data=user_input,
             )
 
@@ -49,16 +49,15 @@ class DDCCIConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
     async def async_step_zeroconf(
         self, discovery_info: DiscoveryInfoType
     ) -> FlowResult:
-        """Handle zeroconf discovery of a HDMI-Control Core HTTP server."""
+        """Handle zeroconf discovery of an HDMI Control Core HTTP server."""
         host = discovery_info["host"]
         port = discovery_info["port"]
 
-        # Prevent duplicates
         await self.async_set_unique_id(f"{host}:{port}")
         self._abort_if_unique_id_configured()
 
         return self.async_create_entry(
-            title=f"HDMI-Control @ {host}:{port}",
+            title=f"HDMI Control @ {host}:{port}",
             data={CONF_HOST: host, CONF_PORT: port},
         )
 
@@ -67,4 +66,3 @@ class DDCCIConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
     def async_get_options_flow(config_entry):
         """If you later add options, return an OptionsFlow here."""
         return None
-
